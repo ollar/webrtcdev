c2b6de00c6f6e8565d6ab27bb6502479fa79881b
Oleg Larkin <olegollar@gmail.com>
2017-03-12 19:05:05 +0300

    docker dev 3

--- server/main.py
+++ server/main.py
@@ -55,7 +55,7 @@ async def RTCServer(websocket, path):
                 await connections[path][message['uid']].close()
                 del connections[path][message['uid']]
 
-start_server = websockets.serve(RTCServer, 'localhost', 8765)
+start_server = websockets.serve(RTCServer, '0.0.0.0', 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()

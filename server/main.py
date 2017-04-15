e786ff9fbac1d6fa5fad99357b78f93bfe9974cb
Oleg Larkin <olegollar@gmail.com>
2017-04-15 20:30:46 +0300

    fix server leak

--- server/main.py
+++ server/main.py
@@ -12,7 +12,6 @@ connections = {}
 async def RTCServer(websocket, path):
     async def channelClose():
         if message.get('uid') in connections[path].keys():
-            # await connections[path][message['uid']].close()
             del connections[path][message['uid']]
             for key, ws in connections[path].items():
                 await ws.send(json.dumps({
@@ -26,47 +25,44 @@ async def RTCServer(websocket, path):
     if not connections.get(path):
         connections[path] = {}
     while True:
-        try:
-            message = await websocket.recv()
-            message = json.loads(message)
-            logging.info('got message {}'.format(message))
+        message = await websocket.recv()
+        message = json.loads(message)
+        logging.info('got message {}'.format(message))
 
-            if message['type'] == 'enterRoom':
-                for key, ws in connections[path].items():
-                    await ws.send(json.dumps({
-                        'type': 'newUser',
-                        'uid': message.get('uid'),
-                    }))
+        if message['type'] == 'enterRoom':
+            for key, ws in connections[path].items():
+                await ws.send(json.dumps({
+                    'type': 'newUser',
+                    'uid': message.get('uid'),
+                }))
 
-                connections[path][message.get('uid')] = websocket
+            connections[path][message.get('uid')] = websocket
 
-            elif message['type'] == 'offer':
-                await connections[path][message.get('toUid')]\
-                    .send(json.dumps({
-                        'type': 'offerFrom',
-                        'fromUid': message.get('fromUid'),
-                        'offer': message.get('offer'),
-                    }))
+        elif message['type'] == 'offer':
+            await connections[path][message.get('toUid')]\
+                .send(json.dumps({
+                    'type': 'offerFrom',
+                    'fromUid': message.get('fromUid'),
+                    'offer': message.get('offer'),
+                }))
 
-            elif message['type'] == 'answer':
-                await connections[path][message.get('toUid')]\
-                    .send(json.dumps({
-                        'type': 'answerFrom',
-                        'fromUid': message.get('fromUid'),
-                        'answer': message.get('answer'),
-                    }))
+        elif message['type'] == 'answer':
+            await connections[path][message.get('toUid')]\
+                .send(json.dumps({
+                    'type': 'answerFrom',
+                    'fromUid': message.get('fromUid'),
+                    'answer': message.get('answer'),
+                }))
 
-            elif message['type'] == 'iceCandidate':
-                await connections[path][message.get('toUid')]\
-                    .send(json.dumps({
-                        'type': 'iceCandidateFrom',
-                        'fromUid': message.get('fromUid'),
-                        'iceCandidate': message.get('iceCandidate'),
-                    }))
+        elif message['type'] == 'iceCandidate':
+            await connections[path][message.get('toUid')]\
+                .send(json.dumps({
+                    'type': 'iceCandidateFrom',
+                    'fromUid': message.get('fromUid'),
+                    'iceCandidate': message.get('iceCandidate'),
+                }))
 
-            elif message['type'] == 'channelClose':
-                await channelClose()
-        except websockets.exceptions.ConnectionClosed:
+        elif message['type'] == 'channelClose':
             await channelClose()
 
 start_server = websockets.serve(RTCServer, '0.0.0.0', 8765)
